43f4f15c63a35ac481bd20eeebde1606e8ce9787
shazly <hmelshazly@gmail.com>
2016-12-05 22:39:28 +0200

    adjust dbs path

--- tornado_server.py
+++ tornado_server.py
@@ -6,13 +6,10 @@ import tornado.ioloop
 import tornado.web
 import simulator
 
-###
-#http://stackoverflow.com/questions/2099728/how-do-i-send-an-ajax-request-on-a-different-port-with-jquery
-###
 
 class GetAWSopts(tornado.web.RequestHandler):
 	def get(self):
-		files = ["/var/www/mcgk/templates/instances_database", "/var/www/mcgk/templates/avail_zones_database"]
+		files = ["history_db/instances_database", "history_db/avail_zones_database"]
 		instances = ""
 		zones = ""
 
